5b4d07ecc391c9eeec6391fe387f19efa9f2aaa9
gurr1 <gustav.engsmyre@gmail.com>
2017-06-18 00:14:55 +0200

    initial code commit

---
+++ src/main/java/Controller/BackgroundController.java
@@ -0,0 +1,8 @@
+package Controller;
+
+/**
+ * Created by gustav on 2017-06-18.
+ */
+public class BackgroundController {
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by gustav on 2017-06-17.
+ */
+public class main {
+    public static void main(String[] args){
+
+    }
+}
